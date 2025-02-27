9c2815ab307a4490815252c31f7ed2c1f16c2b3f
white <2023302111010@whu.edu.cn>
2025-02-27 18:44:16 +0800

    Commit Proj0

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -113,6 +113,68 @@ public class Model extends Observable {
         // TODO: Modify this.board (and perhaps this.score) to account
         // for the tilt to the Side SIDE. If the board changed, set the
         // changed local variable to true.
+        board.setViewingPerspective(side);
+
+        boolean[][] merged = new boolean[board.size()][board.size()];//用于查询该坐标处的tile是否已合并过的二维数组
+        for (int r = 2; r >= 0; r--) {//从第2行开始，向下遍历每一行
+            for (int c = 0; c <= 3; c++) {//遍历第i行的所有列
+                Tile t = board.tile(c, r);//取tile
+
+                if (t != null) {
+                    // 处理当前tile：判断是否需要move，需要move到哪里，是否merge？
+                    // 向上移动，c不变，r加一
+                    int dc = c,dr = r + 1;//取board(c,r+1)，即上面的邻居tile
+                    /*while (dr <= 3) {
+                        Tile dt = board.tile(dc, dr);
+                        if (dt != null) {
+                            // 如果上面的邻居已经被合并过or二者不相等，则不应该移动到(dc,dr)，dr回退一格
+                            if (merged[dc][dr] == true || dt.value() != t.value()) {
+                                dr--;
+                            }
+                            break;
+                        }
+
+                        if (dr == 3) {
+                            break;
+                        }
+
+                        dr++;
+                    }*/
+                    while(dr<=3){
+                        if(dr==4){
+                            dr--;
+                            break;
+                        }
+                        Tile dt = board.tile(dc,dr);
+                        if(dt==null){
+                            dr++;
+                        }else{
+                            break;
+                        }
+                    }
+                    if(dr==4){dr=3;}
+                    if(board.tile(dc,dr)!=null){
+                        if(board.tile(dc,dr).value()!=board.tile(c,r).value()||merged[dc][dr] == true){
+                            dr--;
+                        }
+                    }
+
+
+                    // 如果发生了移动，设置changed
+                    if (dr != r) {
+                        changed = true;
+                    }
+                    // 如果发生了merge
+                    if (board.move(dc, dr, t)) {
+                        merged[dc][dr] = true;//更新二维数组
+                        score += board.tile(dc, dr).value();
+                    }
+                }
+            }
+        }
+
+
+        board.setViewingPerspective(Side.NORTH);
 
         checkGameOver();
         if (changed) {
@@ -137,7 +199,14 @@ public class Model extends Observable {
      *  Empty spaces are stored as null.
      * */
     public static boolean emptySpaceExists(Board b) {
-        // TODO: Fill in this function.
+        int n = b.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (b.tile(i, j) == null) {
+                    return true; // 找到空格立即返回
+                }
+            }
+        }
         return false;
     }
 
@@ -147,7 +216,16 @@ public class Model extends Observable {
      * given a Tile object t, we get its value with t.value().
      */
     public static boolean maxTileExists(Board b) {
-        // TODO: Fill in this function.
+        int n=b.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(b.tile(i,j)!=null) {
+                    if (b.tile(i, j).value() == MAX_PIECE) {
+                        return true;
+                    }
+                }
+            }
+        }
         return false;
     }
 
@@ -158,7 +236,45 @@ public class Model extends Observable {
      * 2. There are two adjacent tiles with the same value.
      */
     public static boolean atLeastOneMoveExists(Board b) {
-        // TODO: Fill in this function.
+        /*int n=b.size();
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-1;j++){
+                if(b.tile(i,j)==null||b.tile(i,j+1)==null||b.tile(i+1,j)==null){return true;}
+                if(b.tile(i,j).value()==b.tile(i,j+1).value()) {
+                    return true;
+                }
+                if(b.tile(i,j).value()==b.tile(i+1,j).value()){
+                    return true;
+                }
+
+            }
+        }
+        return false;*/
+        int size = b.size();
+
+        // Check for empty spaces or adjacent tiles with the same value
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                // Check for empty space
+                if (b.tile(i, j) == null) {
+                    return true;
+                }
+
+                // Check for same value to the right (if not at the rightmost column)
+                if (j < size - 1 && b.tile(i, j) != null && b.tile(i, j + 1) != null
+                        && b.tile(i, j).value() == b.tile(i, j + 1).value()) {
+                    return true;
+                }
+
+                // Check for same value below (if not at the bottom row)
+                if (i < size - 1 && b.tile(i, j) != null && b.tile(i + 1, j) != null
+                        && b.tile(i, j).value() == b.tile(i + 1, j).value()) {
+                    return true;
+                }
+            }
+        }
+
+        // No valid moves exist
         return false;
     }
 
